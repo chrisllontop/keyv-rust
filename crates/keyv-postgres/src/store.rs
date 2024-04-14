7f724741d03e6d0e874d81fa32fa0f60ed37aef8
Abraxas-365 <luisfmiranda8@gmail.com>
2024-04-13 20:12:20 -0500

    feat: adding new funtions of store trait

--- crates/keyv-postgres/src/store.rs
+++ crates/keyv-postgres/src/store.rs
@@ -71,57 +71,41 @@ impl Store for PostgresStore {
 
         Ok(())
     }
+
+    async fn remove_many<T: AsRef<str> + Sync>(&self, keys: &[T]) -> Result<(), StoreError> {
+        // Prepare a vector of &str from the input
+        let keys_str: Vec<&str> = keys.iter().map(|k| k.as_ref()).collect();
+
+        let query = format!("DELETE FROM {} WHERE key = ANY($1)", self.table_name);
+
+        sqlx::query(&query)
+            .bind(&keys_str) // Bind the vector of &str references
+            .execute(&*self.pool)
+            .await
+            .map_err(|_| StoreError::QueryError("Failed to remove the keys".to_string()))?;
+
+        Ok(())
+    }
+
+    async fn clear(&self) -> Result<(), StoreError> {
+        let query = format!("DELETE FROM {}", self.table_name);
+
+        sqlx::query(&query)
+            .execute(&*self.pool)
+            .await
+            .map_err(|_| StoreError::QueryError("Failed to clear the table".to_string()))?;
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
 
+    use keyv::Keyv;
+
     use crate::builder::PostgresStoreBuilder;
 
-    use super::StoreError;
-    use async_trait::async_trait;
-    use keyv::Keyv;
-    use serde_json::Value;
-    use std::collections::HashMap;
-    use std::sync::Mutex;
-
-    pub struct InMemoryStore {
-        db: Mutex<HashMap<String, Value>>,
-    }
-
-    impl InMemoryStore {
-        pub fn new() -> Self {
-            InMemoryStore {
-                db: Mutex::new(HashMap::new()),
-            }
-        }
-    }
-
-    #[async_trait]
-    impl Store for InMemoryStore {
-        async fn initialize(&self) -> Result<(), StoreError> {
-            Ok(())
-        }
-
-        async fn get(&self, key: &str) -> Result<Option<Value>, StoreError> {
-            let db_lock = self.db.lock().unwrap();
-            Ok(db_lock.get(key).cloned())
-        }
-
-        async fn set(&self, key: &str, value: Value, _ttl: Option<u64>) -> Result<(), StoreError> {
-            let mut db_lock = self.db.lock().unwrap();
-            db_lock.insert(key.to_string(), value.clone());
-            Ok(())
-        }
-
-        async fn remove(&self, key: &str) -> Result<(), StoreError> {
-            let mut db_lock = self.db.lock().unwrap();
-            db_lock.remove(key);
-            Ok(())
-        }
-    }
-
-    use super::*;
     #[tokio::test]
     async fn test_keyv_postgres() {
         let store = PostgresStoreBuilder::new()
