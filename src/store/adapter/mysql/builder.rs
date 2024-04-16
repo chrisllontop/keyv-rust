6e198c80fa69b12016fa83b9c5bf833ad0906c03
Luis Fernando Miranda <63959220+Abraxas-365@users.noreply.github.com>
2024-04-15 21:32:36 -0500

    Add MySQL store adapter (#17)

---
+++ src/store/adapter/mysql/builder.rs
@@ -0,0 +1,56 @@
+use sqlx::{mysql::MySqlPoolOptions, MySqlPool};
+use std::sync::Arc;
+
+use crate::{StoreError, DEFAUTL_TABLE_NAME};
+
+use super::MySqlStore;
+
+pub struct MySqlStoreBuilder {
+    uri: Option<String>,
+    pool: Option<Arc<MySqlPool>>,
+    table_name: String,
+}
+
+impl MySqlStoreBuilder {
+    pub fn new() -> Self {
+        Self {
+            uri: None,
+            pool: None,
+            table_name: DEFAUTL_TABLE_NAME.to_string(),
+        }
+    }
+
+    pub fn table_name<S: Into<String>>(mut self, table: S) -> Self {
+        self.table_name = table.into();
+        self
+    }
+
+    pub fn uri<S: Into<String>>(mut self, uri: S) -> Self {
+        self.uri = Some(uri.into());
+        self
+    }
+
+    pub fn pool(mut self, pool: Arc<MySqlPool>) -> Self {
+        self.pool = Some(pool);
+        self
+    }
+
+    pub async fn build(self) -> Result<MySqlStore, StoreError> {
+        let pool = match self.pool {
+            Some(pool) => pool,
+            None => {
+                let uri = self
+                    .uri
+                    .expect("MySqlStore requires either a URI or an existing pool to be set");
+                Arc::new(MySqlPoolOptions::new().connect(&uri).await.map_err(|_| {
+                    StoreError::ConnectionError("Failed to connect to the database".to_string())
+                })?)
+            }
+        };
+
+        Ok(MySqlStore {
+            pool,
+            table_name: self.table_name,
+        })
+    }
+}
---
+++ src/store/adapter/mysql/mod.rs
@@ -0,0 +1,5 @@
+mod builder;
+pub use builder::*;
+
+pub mod mysql;
+pub use mysql::*;
