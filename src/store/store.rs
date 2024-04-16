8dd056e5480568526eb1bf46f42764e485186e63
Abraxas-365 <luisfmiranda8@gmail.com>
2024-04-15 21:44:50 -0500

    chore:refactoring doc comments on store trait

--- src/store/store.rs
+++ src/store/store.rs
@@ -5,28 +5,62 @@ use super::StoreError;
 
 #[async_trait]
 pub trait Store: Send + Sync {
+    /// Initializes the storage backend.
+    /// This method should perform any necessary setup for the storage backend, such as
+    /// establishing database connections or ensuring the existence of required files or schemas.
+    ///
+    /// # Returns
+    /// - `Ok(())` on success.
+    /// - `Err(StoreError)` if initialisation fails.
     async fn initialize(&self) -> Result<(), StoreError>;
 
-    // Retrieves a value based on a key.
-    // The value is expected to be serialized into a string (or potentially bytes),
-    // hence the return type is Result<Option<Value>, KeyvError>.
+    /// Retrieves a value associated with a given key from the store.
+    ///
+    /// # Arguments
+    /// - `key`: A string slice that holds the key for the value to be retrieved.
+    ///
+    /// # Returns
+    /// - `Ok(Some(Value))` if the key exists and the value is successfully retrieved.
+    /// - `Ok(None)` if the key does not exist.
+    /// - `Err(StoreError)` if there is an error retrieving the value.
     async fn get(&self, key: &str) -> Result<Option<Value>, StoreError>;
-    //Evel to use this
-    // async fn get<V>(&self, key: &str) -> Result<Option<V>, KeyvError>
-    // where
-    //     V: DeserializeOwned + Send + Sync;
 
-    // Sets a value for a given key.
-    // Both key and value are strings for simplicity, but could be more complex types
-    // (with proper serialization/deserialization).
-    // The function returns Result<(), KeyvError> to indicate success or error.
+    /// Sets a value for a given key in the store, with an optional time-to-live (TTL).
+    ///
+    /// # Arguments
+    /// - `key`: The key under which the value is stored.
+    /// - `value`: The value to set, represented as a `serde_json::Value`.
+    /// - `ttl`: An optional u64 representing the time-to-live in seconds.
+    ///
+    /// # Returns
+    /// - `Ok(())` if the value is successfully set.
+    /// - `Err(StoreError)` if there is an error setting the value.
     async fn set(&self, key: &str, value: Value, ttl: Option<u64>) -> Result<(), StoreError>;
 
-    /// Removes a key from the store.
-    /// Returns Result<(), KeyvError> to indicate the operation was successful or encountered an error.
+    /// Removes a value associated with a given key from the store.
+    ///
+    /// # Arguments
+    /// - `key`: A string slice that holds the key for the value to be removed.
+    ///
+    /// # Returns
+    /// - `Ok(())` if the key exists and the value is successfully removed.
+    /// - `Err(StoreError)` if there is an error removing the value.
     async fn remove(&self, key: &str) -> Result<(), StoreError>;
 
+    /// Removes multiple values associated with the given keys from the store.
+    ///
+    /// # Arguments
+    /// - `keys`: A slice of string slices representing the keys for the values to be removed.
+    ///
+    /// # Returns
+    /// - `Ok(())` if the values are successfully removed.
+    /// - `Err(StoreError)` if there is an error removing the values.
     async fn remove_many(&self, keys: &[&str]) -> Result<(), StoreError>;
 
+    /// Clears all values from the store.
+    ///
+    /// # Returns
+    /// - `Ok(())` if the store is successfully cleared.
+    /// - `Err(StoreError)` if there is an error clearing the store.
     async fn clear(&self) -> Result<(), StoreError>;
 }
